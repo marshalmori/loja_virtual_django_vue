58d82a541642c49fece9588d8fc5c572143fbdbc
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-10-19 16:49:07 -0300

    Initial commit

---
+++ djackets_vue/src/components/CartItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <tr>
+    <td>
+      <router-link :to="item.product.get_absolute_url">{{
+        item.product.name
+      }}</router-link>
+    </td>
+    <td>R$ {{ item.product.price }}</td>
+    <td>
+      {{ item.quantity }}
+      <a @click="decrementQuantity(item)">-</a>
+      <a @click="incrementQuantity(item)">+</a>
+    </td>
+    <td>R$ {{ getItemTotal(item).toFixed(2) }}</td>
+    <td><button class="delete" @click="removeFromCart(item)"></button></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "CartItem",
+  props: {
+    initialItem: Object,
+  },
+  data() {
+    return {
+      item: this.initialItem,
+    };
+  },
+  methods: {
+    getItemTotal(item) {
+      return item.quantity * item.product.price;
+    },
+    decrementQuantity(item) {
+      item.quantity -= 1;
+      if (item.quantity === 0) {
+        this.$emit("removeFromCart", item);
+      }
+      this.updteCart();
+    },
+    incrementQuantity(item) {
+      item.quantity += 1;
+      this.updteCart();
+    },
+    updteCart() {
+      localStorage.setItem("cart", JSON.stringify(this.$store.state.cart));
+    },
+    removeFromCart(item) {
+      this.$emit("removeFromCart", item);
+      this.updteCart();
+    },
+  },
+};
+</script>
